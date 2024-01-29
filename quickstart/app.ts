1d267b094d6828ddb6fd29880c6007a68c2628f7
zakhaev26 <b422056@iiit-bh.ac.in>
2024-01-29 12:30:23 +0530

    feathers v4 wip

---
+++ quickstart/app.ts
@@ -0,0 +1,72 @@
+import feathers from '@feathersjs/feathers';
+import '@feathersjs/transport-commons';
+import express from '@feathersjs/express';
+import socketio from '@feathersjs/socketio';
+let counter = 0;
+
+interface Message {
+  id: number;
+  text: string;
+}
+
+class MessageService {
+  messages: Message[] = [];
+
+  async find() {
+    return this.messages;
+  }
+
+  async create(data: Pick<Message, 'text'>) {
+    const message: Message = {
+      id: counter,
+      text: data.text
+    };
+
+    counter++;
+    this.messages.push(message);
+
+    return message;
+  }
+
+  async remove(id: number) {
+    console.log("ID : ", id);
+    console.log("DELETE Request Coming.");
+    this.messages = [];
+    return "Done";
+  }
+
+  async hello(req :any, res: ) {
+    res.send('Hello from route handler in class');
+  }
+}
+
+const app = express(feathers());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + `/public`));
+
+app.configure(express.rest());
+app.configure(socketio());
+
+app.use('/messages', new MessageService());
+
+// Register the route handler for /hello from the MessageService class
+const messageService = new MessageService();
+app.use('/hello', messageService.hello.bind(messageService));
+
+app.use(express.errorHandler());
+
+app.on('connection', connection =>
+  app.channel('everybody').join(connection)
+);
+
+app.publish(data => app.channel('everybody'));
+
+app.listen(3030).on('listening', () =>
+  console.log('Feathers server listening on localhost:3030')
+);
+
+app.service('messages').create({
+  text: 'Hello world from the server'
+});
